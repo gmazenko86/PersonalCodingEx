272eec75217919a5aa82ed0975448e6a276a119c
Greg Mazenko <bizcorr9586@att.net>
2020-07-09 20:26:52 -0500

    added FuncProgramExercises

---
+++ src/main/java/funcprogram/FuncProgramExercises.java
@@ -0,0 +1,19 @@
+package funcprogram;
+
+public class FuncProgramExercises {
+
+    public static void main(String... args){
+
+        ConsumerDemo consumerDemo = new ConsumerDemo();
+        consumerDemo.runDemo();
+
+        PredicateDemo predicateDemo = new PredicateDemo();
+        predicateDemo.runDemo();
+
+        SupplierDemo supplierDemo = new SupplierDemo();
+        supplierDemo.runDemo();
+
+        FunctionDemo functionDemo = new FunctionDemo();
+        functionDemo.runDemo();
+    }
+}
