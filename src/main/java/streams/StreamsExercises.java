58312b501cb563d5c7a70ee6cb37fbefec19905e
Greg Mazenko <bizcorr9586@att.net>
2020-07-13 14:08:04 -0500

    Added ReduceDemo

--- src/main/java/streams/StreamsExercises.java
+++ src/main/java/streams/StreamsExercises.java
@@ -5,6 +5,9 @@ public class StreamsExercises {
     static public void main(String... args){
         NumericStreamsDemo nsDemo = new NumericStreamsDemo();
         nsDemo.runDemo();
+
+        ReduceDemo reduceDemo = new ReduceDemo();
+        reduceDemo.runDemo();
     }
 
 }
