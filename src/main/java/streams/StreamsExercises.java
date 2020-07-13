26640daa8710902c78ffce753c5f6251cd08c4f2
Greg Mazenko <bizcorr9586@att.net>
2020-07-13 13:35:12 -0500

    Added NumericStreamsDemo

---
+++ src/main/java/streams/StreamsExercises.java
@@ -0,0 +1,10 @@
+package streams;
+
+public class StreamsExercises {
+
+    static public void main(String... args){
+        NumericStreamsDemo nsDemo = new NumericStreamsDemo();
+        nsDemo.runDemo();
+    }
+
+}
